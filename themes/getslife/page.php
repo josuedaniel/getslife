7dda52ba6937bf298500c3087099d6b548cb4a9e
unknown <josuedaniel831@yahoo.com>
2022-09-03 00:12:35 -0700

    made it so only pages with parent child relationships display the metabox and side pages

--- themes/getslife/page.php
+++ themes/getslife/page.php
@@ -15,7 +15,7 @@ get_header();
     <div class="container container--narrow page-section">
       
         <?php
-        // get the post parent id if it exists, otherwise its 0 which is false
+        // get the child ID if a post parent id exists, otherwise its 0 which is false
             $theParent = wp_get_post_parent_id(get_the_ID());
         //conditional to only show the meta box if the current page is a child page
         //a parent page won't have anything for $theParent 
@@ -39,15 +39,47 @@ get_header();
         ?>
         
 
-        <!--
-        <div class="page-links">
-            <h2 class="page-links__title"><a href="#">About Us</a></h2>
-            <ul class="min-list">
-            <li class="current_page_item"><a href="#">Our History</a></li>
-            <li><a href="#">Our Goals</a></li>
-            </ul>
-        </div> -->
+        <?php
+        
+        //get_pages will return pages in memorydefined by the array we use as a parameter. In this case it will return the current page's children 
+        $testArray = get_pages(array(
+            // Get the current page's children
+            'childof' => get_the_ID(),
 
+            //allows admin to sort child pages in the wordpress admin
+            'sort_column' => 'menu_order'
+        ));
+
+        // conditional to display the side menu pages if the current page is a parent or child
+        if ($theParent or $testArray) { ?>
+
+            <!-- Side menu for pages with parent / child relationships  -->
+            <div class="page-links">
+                <h2 class="page-links__title"><a href="<?php echo get_the_permalink($theParent); ?>"><?php echo get_the_title($theParent); ?></a></h2>
+                <ul class="min-list">
+                    <?php
+                        
+                        // If $theParent exists set $findChildrenOf to $theParent
+                        if ($theParent) {
+                            $findChildrenOf = $theParent;
+                        
+                        // If $theParent doesn't exist set $findChildrenOf to get_the_ID() of the current page
+                        } else {
+                            $findChildrenOf = get_the_ID();
+                        }
+                    //call the wp_list_pages method and add an associative array listing out child pages if they exist
+                        wp_list_pages(array(
+                            
+                            //set the title to null so that we dont get the parent page title
+                            'title_li' => NULL,
+                            
+                            //child_of finds children pages. List child pages if $findChildrenOf is a parent, otherwise dont. 
+                            'child_of'=> $findChildrenOf
+                        ));
+                    ?>
+                </ul>
+            </div> 
+        <?php } ?>
         <div class="generic-content">
             <?php the_content(); ?>
         </div>
