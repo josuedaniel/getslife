8e096ad848f4714636818cb664e08d3cbc72d3c1
unknown <josuedaniel831@yahoo.com>
2022-09-04 11:17:47 -0700

    added dynamic menu functionality support so we can set menus from the wordpress admin

--- themes/getslife/footer.php
+++ themes/getslife/footer.php
@@ -13,23 +13,40 @@
                 <div class="site-footer__col-two">
                     <h3 class="headline headline--small">Explore</h3>
                     <nav class="nav-list">
-                        <ul>
-                        <li><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
-                        <li><a href="#">Learning</a></li>
-                        <li><a href="#">Events</a></li>
-                        <li><a href="#">Gardens</a></li>
-                        </ul>
+                        
+                    <?php
+
+                        //wp function to set the menu to footerLocationOne from the functions file
+                        wp_nav_menu(array(
+                            'theme_location' => 'footerLocationOne'
+                        ));
+                    ?>
+                        <!-- <ul>
+                            <li><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
+                            <li><a href="#">Learning</a></li>
+                            <li><a href="#">Events</a></li>
+                            <li><a href="#">Gardens</a></li>
+                        </ul> -->
                     </nav>
                 </div>
 
                 <div class="site-footer__col-three">
                     <h3 class="headline headline--small">Learn</h3>
                     <nav class="nav-list">
-                    <ul>
+                    
+                    <?php
+
+                        //wp function to set the menu to footerLocationTwo from the functions file
+                        wp_nav_menu(array(
+                            'theme_location' => 'footerLocationTwo'
+                        ));
+                    ?>
+                    
+                    <!--<ul>
                         <li><a href="#">Legal</a></li>
                         <li><a href="<?php echo site_url('/privacy-policy') ?>">Privacy</a></li>
                         <li><a href="#">Careers</a></li>
-                    </ul>
+                    </ul> -->
                     </nav>
                 </div>
             </div>
--- themes/getslife/header.php
+++ themes/getslife/header.php
@@ -13,13 +13,22 @@
       <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
       <div class="site-header__menu group">
         <nav class="main-navigation">
-          <ul>
+
+        <?php
+
+          //wp function to set the menu to headerMenuLocation from the functions file
+          wp_nav_menu(array(
+              'theme_location' => 'headerMenuLocation'
+          ));
+        ?>
+          
+          <!-- <ul>
             <li <?php if (is_page('about-us') or wp_get_post_parent_id(0) == 16) echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
             <li><a href="#">Learning</a></li>
             <li><a href="#">Events</a></li>
             <li><a href="#">Gardens</a></li>
             <li <?php if (get_post_type() == 'post') echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/blog'); ?>">Blog</a></li>
-          </ul>
+          </ul> -->
         </nav>
         <div class="site-header__util">
           <a href="#" class="btn btn--small btn--orange float-left push-right">Login</a>
