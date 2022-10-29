5df9800c4c46da28e455fbf6447311580a9dc90b
unknown <josuedaniel831@yahoo.com>
2022-10-28 21:59:17 -0700

    fixed bug where farmerLandscape custom image sizes were not showing up correctly

--- themes/getslife/single-role.php
+++ themes/getslife/single-role.php
@@ -56,7 +56,7 @@ get_header();
 
                     <li class="professor-card__list-item">
                         <a class="professor-card" href="<?php the_permalink(); ?>">
-                            <img class="professor-card__image" src="<?php the_post_thumbnail(); ?>">
+                            <img class="professor-card__image" src="<?php the_post_thumbnail_url('farmerLandscape'); ?>">
                             <span class="professor-card__name"><?php the_title(); ?></span>
                         </a>
                     </li>
