5b80fcacae7e8a88cb16224a2f4f8093f7aac961
unknown <josuedaniel831@yahoo.com>
2022-11-11 23:54:27 -0700

    Fixed bug in pageBanner function by adding isset to args

--- themes/getslife/functions.php
+++ themes/getslife/functions.php
@@ -1,15 +1,15 @@
 <?php
 
 function pageBanner($args = NULL) {
-    if (!$args['title']) {
+    if (!isset($args['title'])) {
         $args['title'] = get_the_title();
     }
 
-    if (!$args['subtitle']) {
+    if (!isset($args['subtitle'])) {
         $args['subtitle'] = get_field('page_banner_subtitle');
     }
 
-    if (! $args['photo']) {
+    if (!isset($args['photo'])) {
         if (get_field('page_banner_background_image') AND !is_archive() AND !is_home() ) {
             $args['photo'] = get_field('page_banner_background_image')['sizes']['pageBanner'];
         } else {
@@ -19,7 +19,7 @@ function pageBanner($args = NULL) {
     
     ?>
     <div class="page-banner">
-            <div class="page-banner__bg-image" style="background-image: url(<?php echo $args['photo']; ?>)"></div>
+            <div class="page-banner__bg-image" style="background-image: url(<?php $pageBannerImage = get_field('page_banner_background_image'); echo $pageBannerImage['sizes']['pageBanner'] ?>);"></div>
             <div class="page-banner__content container container--narrow">
                 <h1 class="page-banner__title"><?php echo $args['title'] ?></h1>
                 <div class="page-banner__intro">
--- themes/getslife/single-role.php
+++ themes/getslife/single-role.php
@@ -8,7 +8,7 @@ get_header();
             <div class="page-banner__content container container--narrow">
                 <h1 class="page-banner__title"><?php the_title(); ?></h1>
                 <div class="page-banner__intro">
-                <p>DON'T FORGET TO REPLACE ME LATER.</p>
+                <p><?php the_field('page_banner_subtitle'); ?></p>
                 </div>
             </div>
         </div>
