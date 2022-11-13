3f5a4acfaaf9b6683547fe62b6c5a1fc215e962d
unknown <josuedaniel831@yahoo.com>
2022-11-12 22:28:39 -0700

    Adding template parts and removing duplicate code

--- themes/getslife/front-page.php
+++ themes/getslife/front-page.php
@@ -35,29 +35,9 @@
             ));
 
             while ($homePageEvents->have_posts()) {
-              $homePageEvents->the_post(); ?>
-
-              <div class="event-summary">
-                <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
-                  <span class="event-summary__month"><?php 
-                      // create a new DateTime class filled with the event_date from Advanced Custom Fields and assign it to the #eventDate variable
-                    $eventDate = new DateTime(get_field('event_date'));
-
-                    // output the Month part of the event_date variable 
-                    echo $eventDate->format('M');
-                  ?></span>                                  <!-- Output the day part of the $eventDate variable -->
-                  <span class="event-summary__day"><?php echo $eventDate->format('d'); ?></span>
-                </a>
-                <div class="event-summary__content">
-                  <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-                  <p><?php if (has_excerpt()) {
-                                  echo get_the_excerpt();
-                                } else {
-                                  echo wp_trim_words(get_the_content(), 18);
-                                } ?><a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
-                </div>
-              </div>
-              <?php  }
+              $homePageEvents->the_post(); 
+              get_template_part('template-parts/content', 'event');
+            }
           ?>
 
           <p class="t-center no-margin"><a href="<?php echo get_post_type_archive_link('event'); ?>" class="btn btn--blue">View All Events</a></p>
--- themes/getslife/page-past-events.php
+++ themes/getslife/page-past-events.php
@@ -41,24 +41,9 @@ pageBanner(array(
 
         
         while ($pastEvents->have_posts()) {
-            $pastEvents->the_post(); ?>
-            <div class="event-summary">
-            <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
-                <span class="event-summary__month"><?php 
-                    // create a new DateTime class filled with the event_date from Advanced Custom Fields and assign it to the #eventDate variable
-                $eventDate = new DateTime(get_field('event_date'));
-
-                // output the Month part of the event_date variable 
-                echo $eventDate->format('M');
-                ?></span>                                  <!-- Output the day part of the $eventDate variable -->
-                <span class="event-summary__day"><?php echo $eventDate->format('d'); ?></span>
-            </a>
-                <div class="event-summary__content">
-                  <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-                  <p><?php echo wp_trim_words(get_the_content(), 18); ?><a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
-                </div>
-              </div>
-    <?php   } 
+            $pastEvents->the_post();
+            get_template_part('template-parts/content-event');
+        } 
     echo paginate_links(array(
         'total' => $pastEvents->max_num_pages
     )); ?>
